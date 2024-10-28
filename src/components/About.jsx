0a4b6684466c86c4ce38e09730efa0eb1a84ec40
index.ys <ystar5008@naver.com>
2024-10-28 15:21:45 +0900

    Add : 컴포넌트 추가

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm a junior backend developer passionate about API development
+        and improving database structures.
+      </p>
+    </section>
+  );
+}
+
+export default About;
