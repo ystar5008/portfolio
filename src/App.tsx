a4a94436de70aa1f6528a58cb58a93ad2ac8a6cc
unknown <ystar5008@naver.com>
2024-10-28 17:22:24 +0900

    Feat : 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>
+      <main className="container">
         <About />
         <Projects />
         <Contact />
